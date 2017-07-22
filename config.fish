1c192ce8f62c01c3d5884895ce6b844873fec666
Peng Guanwen <pgw1999@outlook.com>
2017-07-22 13:28:54 +0800

    Remove rm alias.

--- config.fish
+++ config.fish
@@ -1,2 +1,2 @@
 set fish_greeting "Login at "(date)
-alias rm 'echo This is not the command you are looking for. Try trash instead. ; /bin/false'
+# alias rm 'echo This is not the command you are looking for. Try trash instead. ; /bin/false'
