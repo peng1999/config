c23ea2d4623f7e6f2badec7aaa51954f9757c1be
Peng Guanwen <pg999w@outlook.com>
2021-01-03 11:29:22 +0800

    [fish] Comment out unneeded configs

--- config.fish
+++ config.fish
@@ -1,3 +1,4 @@
-set fish_greeting "Login at "(date)
+# set fish_greeting "Login at "(date)
 # alias rm 'echo This is not the command you are looking for. Try trash instead. ; /bin/false'
 set -x EDITOR nvim
+
